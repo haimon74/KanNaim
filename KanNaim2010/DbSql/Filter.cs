a516bec4f3f5a62be21a83d56e09787ad4c65c3c
Haim <haim.azar@gmail.com>
2010-01-05 09:44:16 +0200

    forgot to add DbSql library

---
+++ KanNaim2010/DbSql/Filter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Linq;
+
+namespace DbSql
+{
+    public class Filter
+    {
+        private static readonly DataClassesKanNaimDataContext Db = new DataClassesKanNaimDataContext();
+
+        public static IQueryable<Table_OriginalPhotosArchive> GetOriginalPhotosByCategoryName(string catName)
+        {
+            int catId = Lookup.GetLookupCategoryIdFromName(catName);
+
+            return from c in Db.Table_OriginalPhotosArchives
+                   where c.CategoryId == catId
+                   select c;
+        }
+
+        public static IQueryable<Table_PhotosArchive> GetPhotosArchiveByOriginalPhotoId(int originalPhotoId)
+        {
+            return from c in Db.Table_PhotosArchives
+                   where c.OriginalPhotoId == originalPhotoId
+                   select c;
+        }
+
+        public static int GetOriginalPhotoIdByPhotoName(string photoName)
+        {
+            try
+            {
+                return (from c in Db.Table_OriginalPhotosArchives
+                        where c.Name == photoName
+                        select c).Single().PhotoId;
+            }
+            catch(Exception exception)
+            {
+                return -1;
+            }
+        }
+
+        public static Table_OriginalPhotosArchive GetOriginalPhotoFromId(int id)
+        {
+            var result = from c in Db.Table_OriginalPhotosArchives
+                         where c.PhotoId == id
+                         select c;
+            try
+            {
+                return result.First();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static IQueryable<Table_User> GetUserByUserNameOrPhone(string str)
+        {
+            return (from c in Db.Table_Users
+                    where (c.PhoneNumber == str || c.UserName == str)
+                    select c);
+        }
+        public static int GetUserIdFromUserNameOrPhone(string str)
+        {
+            var result = GetUserByUserNameOrPhone(str);
+            return result.First().UserId;
+        }
+        public static string GetUserRoleIdFromUserNameOrPhone(string str)
+        {
+            var result = GetUserByUserNameOrPhone(str);
+            int roleId = result.First().RoleId;
+            try
+            {
+                return (from c in Db.Table_LookupRoles
+                        where roleId == c.RoleId
+                        select c.RoleDescription).First();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public static IQueryable<Table_Taktzirim> GetAllTaktzirim()
+        {
+            return from c in Db.Table_Taktzirims
+                   select c;
+        }
+        public static IQueryable<Table_Taktzirim> GetTaktzirimByTakTypeId(int id)
+        {
+            return from c in Db.Table_Taktzirims
+                   where c.TakTypeId == id
+                   select c;
+        }
+        public static IQueryable<Table_Taktzirim> GetTaktzirimByArticleId(int id)
+        {
+            return from c in Db.Table_Taktzirims
+                   where c.ArticleId == id
+                   select c;
+        }
+        
+        public static Table_VideosArchive GetVideoFromId(int id)
+        {
+            var result = from c in Db.Table_VideosArchives
+                         where c.Id == id
+                         select c;
+            try
+            {
+                return result.First();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static Table_LinksPageBottom GetPageBottomLinkByLinkId(int id)
+        {
+            var result =  from c in Db.Table_LinksPageBottoms
+                           where c.LinkId == id
+                           select c;
+            try
+            {
+                return result.First();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public static Table_LinksPrefered GetPreferedLinkByLinkId(int id)
+        {
+            var result = from c in Db.Table_LinksPrefereds
+                         where c.LinkId == id
+                         select c;
+            try
+            {
+                return result.First();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        
+    }
+}
---
+++ KanNaim2010/DbSql/Lookup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace DbSql
+{
+    public class Lookup
+    {
+        public static readonly DataClassesKanNaimDataContext Db = new DataClassesKanNaimDataContext();
+
+        public static IQueryable<Table_LookupPhotoType> GetLookupPhotoTypes()
+        {
+            return from c in Db.Table_LookupPhotoTypes
+                   select c;
+        }
+
+        public static int GetLookupCategoryIdFromName(string name)
+        {
+            try
+            {
+                var result = (from c in Db.Table_LookupCategories
+                              where c.CatEnglishName == name || c.CatHebrewName == name
+                              select c).Single().CatId;
+                return result;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+        }
+
+        public static string GetLookupCategoryNameFromId(int id)
+        {
+            try
+            {
+                var result = (from c in Db.Table_LookupCategories
+                              where c.CatId == id
+                              select c).Single().CatHebrewName;
+                return result;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        public static int GetLookupReporterIdFromName(string name)
+        {
+            try
+            {
+                var result = (from c in Db.Table_LookupReporters
+                              where c.PublishNameLong == name || c.PublishNameShort == name
+                              select c).Single().UserId;
+                return result;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+        public static Table_LookupReporter GetLookupReporterFromUserId(int id)
+        {
+            return (from c in Db.Table_LookupReporters
+                    where c.UserId == id
+                    select c).Single();
+        }
+    }
+}
